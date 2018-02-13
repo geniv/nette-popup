6b5702d967411e8969082ce9c2196361cf0e59f5
Martin[cze] Fugess <martin.fugess@gmail.com>
2018-02-13 21:23:33 +0100

    upravy pro realne pouziti

--- src/Popup.js
+++ src/Popup.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
     if ($(".nette-popup")[0]) {
-        $("body").addClass("nette-popup-on");
+        $("body").addClass("nette-popup--shown");
     }
-    $(".nette-popup-manual-close").on("click", function(event) {
-        $(".nette-popup.on").addClass("out end").removeClass("on"); // nejspis se vubec nema jak provest
-        $("body").removeClass("nette-popup-on");
+    $(".nette-popup__close--manual").on("click", function(event) {
+        $("body").removeClass("nette-popup--shown");
     });
 });
